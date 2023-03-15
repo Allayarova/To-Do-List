18454808479cf7d91685a7e1a297c04d8817c341
Mahliyo <budur1202@gmail.com>
2023-03-15 17:18:47 +0300

    add list

---
+++ script.js
@@ -0,0 +1,36 @@
+"use script";
+
+const form = document.getElementById("new-task-form");
+
+const input = document.getElementById("new-task-input");
+
+const list_el = document.querySelector(".task");
+
+const todobutton = document.querySelector(".btn");
+
+const actions = document.querySelector(".actions");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const task = input.value;
+
+  if (!task) {
+    alert("Please fill out the task");
+    return;
+  }
+
+  let paragraph = document.createElement("div");
+  paragraph.innerText = input.value;
+  input.value = "";
+  actions.appendChild(paragraph);
+  paragraph.style.fontFamily = "cursive";
+
+  paragraph.addEventListener("click", () => {
+    paragraph.style.textDecoration = "line-through";
+  });
+
+  paragraph.addEventListener("dblclick", () => {
+    actions.removeChild(paragraph);
+  });
+});
